ab1447580586d2b6925dd4f6afde67517690623e
lovregraso <lg52480@fer.hr>
2024-09-13 14:02:44 +0200

    lidar modeling

--- Scripts/Sensors/Lidar/RaycastLidar.cs
+++ Scripts/Sensors/Lidar/RaycastLidar.cs
@@ -20,7 +20,12 @@ using System.Linq;
 using System;
 using Marus.ObjectAnnotation;
 using Marus.Utils;
-
+using System.IO;
+using UnityEngine.Rendering;
+using UnityEngine.Serialization;
+using UnityEngine.Rendering.HighDefinition;
+using System;
+using System.Threading.Tasks;
 namespace Marus.Sensors
 {
 
@@ -66,8 +71,8 @@ namespace Marus.Sensors
 
         public NativeArray<Vector3> Points;
         public NativeArray<LidarReading> Readings;
-
-
+        public NativeArray<Vector3> FilteredPoints;
+        public NativeArray<LidarReading> FilteredReadings;
         PointCloudManager _pointCloudManager;
         RaycastJobHelper<LidarReading> _raycastHelper;
         Coroutine _coroutine;
@@ -84,6 +89,18 @@ namespace Marus.Sensors
         private Dictionary<int, int> colliderLayer;
 
         public LayerMask blackHoleLayers;
+        // modified for fog
+       //**********************************************************************
+        public Volume volume;
+        Fog fog;
+        private float fogAttenuationDistance;
+
+        RainIntensity rain;
+
+        private float rainIntensity;
+
+        int counter;
+        
 
         void Start()
         {
@@ -127,13 +144,30 @@ namespace Marus.Sensors
             _pointCloudManager = PointCloudManager.CreatePointCloud(gameObject, name + "_PointCloud", totalRays, ParticleMaterial, pointCloudShader);
             _coroutine = StartCoroutine(_raycastHelper.RaycastInLoop());
 
-        }
+            // fog
+            volume.profile.TryGet(out fog);
+            fogAttenuationDistance = fog.meanFreePath.value;
 
+            // rain
+            rain = FindObjectOfType<RainIntensity>();
+
+            if (rain != null)
+            {
+                rainIntensity = rain.Intensity;
+            }
+            else{
+                rainIntensity = 0;
+            }
+            
+
+        }
+       
         protected override void SampleSensor()
         {
             _pointCloudManager.UpdatePointCloud(Points);
             _raycastHelper.SampleFrequency = SampleFrequency;
         }
+        private int fileCounter = 1; // Brojač datoteka
 
         private void OnFinish(NativeArray<Vector3> points, NativeArray<LidarReading> readings)
         {
@@ -141,7 +175,84 @@ namespace Marus.Sensors
             readings.CopyTo(this.Readings);
             Log(new {points});
             hasData = true;
+
+            // filter detected points and readings in foggy conditions
+
+            (NativeArray<LidarReading> fogFilteredReadings, NativeArray<Vector3> fogFilteredPoints) = FogFilteredReadings(readings, points);
+            
+          
+            (NativeArray<LidarReading> rainFilteredReadings, NativeArray<Vector3> rainFilteredPoints) = RainFilteredReadings(fogFilteredReadings, fogFilteredPoints);
+            
+
+            // save intensity in files...
+
+            string folderPath = @"C:\Users\lovre\Git\LidarData"; // Putanja do foldera
+            string fileName = $"intenzitet{fileCounter}.txt"; // Ime datoteke
+            string filePath = Path.Combine(folderPath, fileName); // Putanja do datoteke
+
+            // Ako folder ne postoji, kreiraj ga
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            // Upisivanje podataka u datoteku
+            var combined = rainFilteredPoints.Zip(rainFilteredReadings, (p, i) => new { p, i });
+            using (StreamWriter writer = File.CreateText(filePath))
+            {   
+                
+               foreach (var point_int in combined)
+                {
+                  writer.WriteLine($"x: {point_int.p.x}, y: {point_int.p.y}, z: {point_int.p.z}, I: {point_int.i.Intensity}");
+                   
+                }
+            }
+            
+            fileCounter++; // increase counter value
+          
+            string folderPathPCD = @"C:\Users\lovre\Git\PCD"; // set path to folder
+            string fileNamePCD = $"point_cloud{fileCounter}.pcd"; // file name
+            string filePathPCD = Path.Combine(folderPathPCD, fileNamePCD); // combine
+            if (!Directory.Exists(folderPathPCD))
+            {
+                Directory.CreateDirectory(folderPathPCD);
+            }
+            PCDSaver.WriteToPcdFileWithIntensity(filePathPCD, rainFilteredPoints.ToList(), rainFilteredReadings.ToList());
+            //ExportToPCD(fileName, rainFilteredPoints, rainFilteredReadings);
+    
         }
+
+        private static void WriteToPcdFileWithIntensity(string filePath, NativeArray<Vector3> pointcloud, NativeArray<LidarReading> lidarReadings)
+        {
+            string metadata = "VERSION .7\n" +
+                "FIELDS x y z intensity\n" +
+                "SIZE 4 4 4 4\n" +
+                "TYPE F F F U\n" +
+                "COUNT 1 1 1 1\n" +
+                "WIDTH " + pointcloud.Length.ToString() + "\n" +
+                "HEIGHT 1\n" +
+                "VIEWPOINT 0 0 0 1 0 0 0\n" +
+                "POINTS " + pointcloud.Length.ToString() + "\n" +
+                "DATA binary\n";
+
+            var pointSize = 16; // modified
+            File.WriteAllText(filePath, metadata);
+            using (var fileStream = new FileStream(filePath, FileMode.Append, FileAccess.Write, FileShare.None))
+            using (var bw = new BinaryWriter(fileStream))
+            {   
+                byte[] bytes = new byte[pointcloud.Length * pointSize];
+                for(int i = 0; i < pointcloud.Length; i++)
+                {   
+                    //pointSize = (int)lidarReadings[i].Intensity/255;
+                    Buffer.BlockCopy( BitConverter.GetBytes( pointcloud[i].x ), 0, bytes, i*pointSize, 4 );
+                    Buffer.BlockCopy( BitConverter.GetBytes( pointcloud[i].z ), 0, bytes, i*pointSize + 4, 4 );
+                    Buffer.BlockCopy( BitConverter.GetBytes( pointcloud[i].y ), 0, bytes, i*pointSize + 8, 4 );
+                    Buffer.BlockCopy( BitConverter.GetBytes( lidarReadings[i].Intensity ), 0, bytes, i*pointSize + 12, 4 );
+                }
+                bw.Write(bytes);
+            }
+        }
+        
 
         private LidarReading OnLidarHit(RaycastHit hit, Vector3 direction, int index)
         {
@@ -167,8 +278,166 @@ namespace Marus.Sensors
                     reading.IsValid = false;
                 }
             }
-            reading.Intensity = 255;
+            //modified --------------------------------------------------------------------------
+            var distance = hit.distance;
+           // reading.Intensity = 255;
+            
+            float dotProduct = Vector3.Dot(hit.normal, direction);
+
+            // calculate magnitude of direction and hit.normal
+            float magnitudeNormale = hit.normal.magnitude;
+            float magnitudeDirection = direction.magnitude;
+
+            // calculate cosine between hit.normale and direction
+            float cosine = Math.Abs(dotProduct / (magnitudeNormale * magnitudeDirection));
+            
+            double number = Math.Round(255f / Math.Pow(distance,2))*cosine;
+            
+            int constant = 700;
+            reading.Intensity = (ushort)(Math.Round(constant*(255f / Math.Pow(2*distance,2))));
+            if (reading.Intensity > 255){
+                reading.Intensity = 255;
+            }
+
+            //-----------------------------------------------------------------------------------------
+            // adjusting intensity in foggy condition
+            
+            reading = FogIntensityReduction(reading);
+
+            // adjusting intensity in rainy condition
+            
+            if (rain != null)
+            {
+                reading = RainIntensityReduction(reading);
+            }
+            
+            /*
+            //filtering readings in foggy condition in terms of detection range
+            double dist =  distance;
+            double detectionRange = CalcDetectionRange(distance);
+            if(detectionRange < distance){
+                reading.IsValid = false;
+            }
+            */
+
             return reading;
+        }
+
+        private LidarReading FogIntensityReduction(LidarReading reading){
+            
+            var eta_fog = (1.01 + 0.15*fogAttenuationDistance)/80;
+            if (eta_fog > 1)
+            {
+                eta_fog = 1;
+            }
+            reading.Intensity = (ushort)(reading.Intensity*eta_fog);
+            return reading;
+       }
+
+          private LidarReading RainIntensityReduction(LidarReading reading){
+            
+            var eta_rain = (18.585 - 2.356*rainIntensity + 0.062*Math.Pow(rainIntensity,2))/ 18.585;
+            if (rainIntensity > 10)
+            {
+                eta_rain = 0;
+            }
+            reading.Intensity = (ushort)(reading.Intensity*eta_rain);
+            return reading;
+       }
+
+       /*
+
+        private double CalcDetectionRange(double dist){
+            double range = 3.653 + 1.497*fogAttenuationDistance - 0.00258*Math.Pow(fogAttenuationDistance,2);
+            if(range > 200){
+                range = 200;
+            }
+            return range;
+            
+       }
+        */
+
+        private (NativeArray<LidarReading>, NativeArray<Vector3>) FogFilteredReadings(NativeArray<LidarReading> readings, NativeArray<Vector3> points){
+
+            double p = 0.00032*Math.Pow(fogAttenuationDistance,2) - 0.0164*fogAttenuationDistance + 0.167; // calculate percentage of points that remain
+            if (p > 1){
+                return (readings, points);
+            }
+            if (p <0){
+                p = 0;
+            }
+            int n = readings.Length;
+            int m = (int)Math.Round(n*p);
+
+            FilteredReadings = new NativeArray<LidarReading>(m, Allocator.Temp);
+            FilteredPoints = new NativeArray<Vector3>(m, Allocator.Temp);            
+            
+            List<int> randomNumbers = new List<int>();
+            HashSet<int> set = new HashSet<int>();
+
+            System.Random rand = new System.Random(Guid.NewGuid().GetHashCode());
+
+            while (set.Count < m)
+            {
+                int randomNumber = rand.Next(0,n);
+                set.Add(randomNumber);
+            }
+
+            // Pretvori HashSet u listu
+            List<int> randomNumberList = new List<int>(set);
+
+            int i = 0;
+
+            foreach(int r in randomNumberList){
+                FilteredReadings[i] = readings[r];
+                FilteredPoints[i] = points[r];
+                i++;
+            }
+           
+            return (FilteredReadings, FilteredPoints);
+        }
+
+        private (NativeArray<LidarReading>, NativeArray<Vector3>) RainFilteredReadings(NativeArray<LidarReading> readings, NativeArray<Vector3> points){
+            if (rain = null){
+                return (readings, points);
+            }
+
+            double p = (270 - 12.384*rainIntensity - 1.361*Math.Pow(rainIntensity,2))/270; // calculate percentage of points that remain
+            if (p < 0){
+                p = 0;
+            }
+            if (p >= 1){
+                return (readings, points);
+            }
+            int n = readings.Length;
+            int m = (int)Math.Round(n*p);
+
+            FilteredReadings = new NativeArray<LidarReading>(m, Allocator.Temp);
+            FilteredPoints = new NativeArray<Vector3>(m, Allocator.Temp);            
+            
+            List<int> randomNumbers = new List<int>();
+            HashSet<int> set = new HashSet<int>();
+
+            System.Random rand = new System.Random(Guid.NewGuid().GetHashCode());
+
+            while (set.Count < m)
+            {
+                int randomNumber = rand.Next(0,n);
+                set.Add(randomNumber);
+            }
+
+            // Pretvori HashSet u listu
+            List<int> randomNumberList = new List<int>(set);
+
+            int i = 0;
+
+            foreach(int r in randomNumberList){
+                FilteredReadings[i] = readings[r];
+                FilteredPoints[i] = points[r];
+                i++;
+            }
+           
+            return (FilteredReadings, FilteredPoints);
         }
 
         /// <summary>
@@ -241,6 +510,9 @@ namespace Marus.Sensors
             Points.Dispose();
             Readings.Dispose();
             _rayAngles.Dispose();
+          
+            
+
         }
     }
 
